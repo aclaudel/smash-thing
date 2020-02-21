e1e35179deb53f18f22703d4610451433be8ed6c
aclaudel <arnaud.claudel@codurance.com>
2020-02-21 17:13:09 +0000

    orientation service added

---
+++ src/orientation/OrientationService.test.ts
@@ -0,0 +1,19 @@
+import OrientationService from "./OrientationService";
+
+describe("Orientation service", () => {
+    const service = new OrientationService();
+
+    it("should return the left orientation", () => {
+        expect(service.left("NORTH")).toBe("WEST");
+        expect(service.left("WEST")).toBe("SOUTH");
+        expect(service.left("SOUTH")).toBe("EAST");
+        expect(service.left("EAST")).toBe("NORTH");
+    });
+
+    it("should return the right orientation", () => {
+        expect(service.right("NORTH")).toBe("EAST");
+        expect(service.right("EAST")).toBe("SOUTH");
+        expect(service.right("SOUTH")).toBe("WEST");
+        expect(service.right("WEST")).toBe("NORTH");
+    });
+});
---
+++ src/orientation/OrientationService.ts
@@ -0,0 +1,22 @@
+import {Orientation} from "./Orientation";
+
+export default class OrientationService {
+
+    left(orientation: Orientation) {
+        switch (orientation) {
+            case "NORTH": return "WEST";
+            case "WEST": return "SOUTH";
+            case "SOUTH": return "EAST";
+            case "EAST": return "NORTH";
+        }
+    }
+
+    right(orientation: Orientation) {
+        switch (orientation) {
+            case "NORTH": return "EAST";
+            case "EAST": return "SOUTH";
+            case "SOUTH": return "WEST";
+            case "WEST": return "NORTH";
+        }
+    }
+}
