41ef7ebe7cf2c8a274f5d42cbb662991076b5203
aclaudel <arnaud.claudel@codurance.com>
2020-02-21 18:01:45 +0000

    refactor test to add more cases

--- src/character/Character.test.ts
+++ src/character/Character.test.ts
@@ -2,12 +2,16 @@ import Position from "../position/Position";
 import CharacterState from "./CharacterState";
 
 describe("Character", () => {
-    it("should move up if facing north", () => {
-        const initPosition = Position.of(1,1);
+    const initPosition = Position.of(1,1);
+
+    describe("when facing North", () => {
         const initOrientation = "NORTH";
-
         const characterState = CharacterState.at(initPosition, initOrientation);
+        const nextCharacterState = characterState.move();
 
-        expect(characterState.move().position.y).toBe(initPosition.y +1);
+        it("should move up", () => {
+            expect(nextCharacterState.position.y).toBe(initPosition.y +1);
+        });
+
     });
 });
