e4fee59fe7dc2067af3fa7464e0be048dbe714b8
aclaudel <arnaud.claudel@codurance.com>
2020-02-21 18:14:30 +0000

    verifying that the returned position is used

--- src/character/Character.test.ts
+++ src/character/Character.test.ts
@@ -1,9 +1,10 @@
 import Position from "../position/Position";
 import CharacterState from "./CharacterState";
-import {instance, mock, resetCalls, verify} from "ts-mockito";
+import {instance, mock, resetCalls, verify, when} from "ts-mockito";
 
 describe("Character", () => {
     const positionMock = mock<Position>();
+    const nextPosition: Position = Position.of(0,0); // could be any
 
     beforeEach(() => {
         resetCalls(positionMock);
@@ -13,10 +14,13 @@ describe("Character", () => {
         const initOrientation = "NORTH";
         const characterState = CharacterState.at(instance(positionMock), initOrientation);
 
+        when(positionMock.transY(1))
+            .thenReturn(nextPosition);
 
         it("should move up", () => {
-            characterState.move();
+            const nextCharacterState = characterState.move();
             verify(positionMock.transY(1)).called();
+            expect(nextCharacterState.position).toBe(nextPosition);
         });
     });
 });
