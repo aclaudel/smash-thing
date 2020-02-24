482344d31dded60b52404574a60b6948892790d9
aclaudel <arnaud.claudel@codurance.com>
2020-02-24 12:47:51 +0000

    add acceptance (and harness) test before refactoring

---
+++ src/components/App.acceptance.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {render} from "@testing-library/react";
+import App from "./App";
+import GameEngine from "../core/game/GameEngine";
+import userEvent from "@testing-library/user-event";
+import Character from "../core/character/Character";
+
+describe("Acceptance test", () => {
+    it("should update the character state when command buttons are clicked ", () => {
+        const gameEngine = new GameEngine();
+        const character = new Character("id-1", gameEngine);
+        gameEngine.addCharacter(character);
+
+        const app = render(<App gameEngine={gameEngine}/>);
+
+        const moveButton = app.getByTestId("move-button");
+        const leftButton = app.getByTestId("left-button");
+        const rightButton = app.getByTestId("right-button");
+
+        userEvent.click(moveButton);
+        userEvent.click(rightButton);
+        userEvent.click(rightButton);
+        userEvent.click(leftButton);
+
+        const characterInfo = app.getByTestId("character-info-id-1");
+        expect(characterInfo).toHaveTextContent("id-1: EAST - 0 1");
+    });
+});
